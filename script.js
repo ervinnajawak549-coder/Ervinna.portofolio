bb7683440db558eccffe8885cccd26f61058f142
ervinnajawak549-coder <ervinnajawak549@gmail.com>
2026-09-17 16:10:10 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -7,23 +7,31 @@ function hitungNilai() {
     let nilaiAkhir = (tugas * 0.30) + (uts * 0.30) + (uas * 0.40);
 
     let grade;
+    let keterangan;
 
     if (nilaiAkhir >= 85) {
         grade = "A";
+        keterangan = "Sangat Baik";
     } else if (nilaiAkhir >= 75) {
         grade = "B";
+        keterangan = "Baik";
     } else if (nilaiAkhir >= 65) {
         grade = "C";
+        keterangan = "Cukup";
     } else if (nilaiAkhir >= 50) {
         grade = "D";
+        keterangan = "Kurang";
     } else {
         grade = "E";
+        keterangan = "Tidak Lulus";
     }
 
     document.getElementById("nilaiAkhir").textContent =
         nilaiAkhir.toFixed(2);
 
     document.getElementById("grade").textContent = grade;
+
+    document.getElementById("keterangan").textContent = keterangan;
 
     document.getElementById("hasil").classList.remove("hidden");
 }
